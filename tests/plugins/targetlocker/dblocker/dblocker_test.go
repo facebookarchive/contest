71c19f4808f44c369aee62622b5d19607def32d2
Tobias Fleig <tfleig@fb.com>
2020-08-17 16:13:07 +0100

    simple db based target locker
    Summary of changes:
     - Add a minimal rdbms-based target locker implementation
     - Clarify target.Locker API docs
    
    
    This adds a minimal target locker that uses a relational database as backend.
    
    Using this locker allows sharing locks between multiple instances of Contest Server and makes locks survive restarts.
    
    The unit/integration test suite for this new locker is an extended version of the inmemory test suite, some tests changed to confirm with the clarified locker API and some tests added to prevent bugs like #134.

---
+++ tests/plugins/targetlocker/dblocker/dblocker_test.go
@@ -0,0 +1,199 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+// +build integration_storage
+
+package dblocker
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	"github.com/facebookincubator/contest/pkg/logging"
+	"github.com/facebookincubator/contest/pkg/target"
+	"github.com/facebookincubator/contest/pkg/types"
+	"github.com/facebookincubator/contest/plugins/targetlocker/dblocker"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	jobID = types.JobID(123)
+
+	targetOne  = target.Target{Name: "target001", ID: "001"}
+	targetTwo  = target.Target{Name: "target002", ID: "002"}
+	oneTarget  = []*target.Target{&targetOne}
+	twoTargets = []*target.Target{&targetOne, &targetTwo}
+
+	tl *dblocker.DBLocker
+)
+
+func TestMain(m *testing.M) {
+	// tests reset the database, which makes the locker yell all the time,
+	// disable for the integration tests
+	logging.GetLogger("tests/integ")
+	logging.Disable()
+
+	dbURI := "contest:contest@tcp(mysql:3306)/contest_integ?parseTime=true"
+	locker, err := dblocker.New(dbURI, 2*time.Second, 2*time.Second)
+	if err != nil {
+		panic(err)
+	}
+	tl = locker.(*dblocker.DBLocker)
+	os.Exit(m.Run())
+}
+
+func TestNew(t *testing.T) {
+	tl.ResetAllLocks()
+	assert.NotNil(t, tl)
+	assert.IsType(t, &dblocker.DBLocker{}, tl)
+}
+
+func TestLockInvalidJobIDAndNoTargets(t *testing.T) {
+	tl.ResetAllLocks()
+	assert.Error(t, tl.Lock(0, nil))
+}
+
+func TestLockValidJobIDAndNoTargets(t *testing.T) {
+	tl.ResetAllLocks()
+	assert.NoError(t, tl.Lock(jobID, nil))
+}
+
+func TestLockValidJobIDAndNoTargets2(t *testing.T) {
+	tl.ResetAllLocks()
+	assert.NoError(t, tl.Lock(jobID, []*target.Target{}))
+}
+
+func TestLockInvalidJobIDAndOneTarget(t *testing.T) {
+	tl.ResetAllLocks()
+	assert.Error(t, tl.Lock(0, oneTarget))
+}
+
+func TestLockValidJobIDAndEmptyIDTarget(t *testing.T) {
+	tl.ResetAllLocks()
+	assert.Error(t, tl.Lock(jobID, []*target.Target{&target.Target{Name: "test", ID: ""}}))
+}
+
+func TestLockValidJobIDAndOneTarget(t *testing.T) {
+	tl.ResetAllLocks()
+	assert.NoError(t, tl.Lock(jobID, oneTarget))
+}
+
+func TestLockValidJobIDAndTwoTargets(t *testing.T) {
+	tl.ResetAllLocks()
+	assert.NoError(t, tl.Lock(jobID, twoTargets))
+}
+
+func TestLockReentrantLock(t *testing.T) {
+	tl.ResetAllLocks()
+	assert.NoError(t, tl.Lock(jobID, twoTargets))
+	assert.NoError(t, tl.Lock(jobID, oneTarget))
+	assert.NoError(t, tl.Lock(jobID, twoTargets))
+}
+
+func TestLockReentrantLockDifferentJobID(t *testing.T) {
+	tl.ResetAllLocks()
+	assert.NoError(t, tl.Lock(jobID, twoTargets))
+	assert.Error(t, tl.Lock(jobID+1, twoTargets))
+	assert.Error(t, tl.Lock(jobID+1, oneTarget))
+}
+
+func TestUnlockInvalidJobIDAndNoTargets(t *testing.T) {
+	tl.ResetAllLocks()
+	assert.NoError(t, tl.Unlock(jobID, nil))
+}
+
+func TestUnlockValidJobIDAndNoTargets(t *testing.T) {
+	tl.ResetAllLocks()
+	assert.NoError(t, tl.Unlock(jobID, nil))
+}
+
+func TestUnlockInvalidJobIDAndOneTarget(t *testing.T) {
+	tl.ResetAllLocks()
+	assert.Error(t, tl.Unlock(0, oneTarget))
+}
+
+func TestUnlockValidJobIDAndOneTarget(t *testing.T) {
+	tl.ResetAllLocks()
+	assert.NoError(t, tl.Unlock(jobID, oneTarget))
+}
+
+func TestUnlockValidJobIDAndTwoTargets(t *testing.T) {
+	tl.ResetAllLocks()
+	assert.NoError(t, tl.Unlock(jobID, twoTargets))
+}
+
+func TestLockUnlockSameJobID(t *testing.T) {
+	tl.ResetAllLocks()
+	assert.NoError(t, tl.Lock(jobID, twoTargets))
+	assert.NoError(t, tl.Unlock(jobID, twoTargets))
+}
+
+func TestLockUnlockDifferentJobID(t *testing.T) {
+	tl.ResetAllLocks()
+	assert.NoError(t, tl.Lock(jobID, twoTargets))
+	// this does not error, but will also not release the lock...
+	assert.NoError(t, tl.Unlock(jobID+1, twoTargets))
+	// ... so it cannot be acquired by job+1
+	assert.Error(t, tl.Lock(jobID+1, twoTargets))
+}
+
+func TestRefreshLocks(t *testing.T) {
+	tl.ResetAllLocks()
+	assert.NoError(t, tl.RefreshLocks(jobID, twoTargets))
+}
+
+func TestRefreshLocksTwice(t *testing.T) {
+	tl.ResetAllLocks()
+	assert.NoError(t, tl.RefreshLocks(jobID, twoTargets))
+	assert.NoError(t, tl.RefreshLocks(jobID, twoTargets))
+}
+
+func TestRefreshLocksOneThenTwo(t *testing.T) {
+	tl.ResetAllLocks()
+	assert.NoError(t, tl.RefreshLocks(jobID, oneTarget))
+	assert.NoError(t, tl.RefreshLocks(jobID, twoTargets))
+}
+
+func TestRefreshLocksTwoThenOne(t *testing.T) {
+	tl.ResetAllLocks()
+	assert.NoError(t, tl.RefreshLocks(jobID, twoTargets))
+	assert.NoError(t, tl.RefreshLocks(jobID, oneTarget))
+}
+
+func TestLockExpiry(t *testing.T) {
+	tl.ResetAllLocks()
+	assert.NoError(t, tl.Lock(jobID, twoTargets))
+	// getting them immediately fails for other owner
+	assert.Error(t, tl.Lock(jobID+1, twoTargets))
+	time.Sleep(3 * time.Second)
+	// expired, now it should work
+	assert.NoError(t, tl.Lock(jobID+1, twoTargets))
+}
+
+func TestRefreshMultiple(t *testing.T) {
+	// not super happy with this test, it is timing sensitive
+	tl.ResetAllLocks()
+	// now for the actual test
+	assert.NoError(t, tl.Lock(jobID, twoTargets))
+	time.Sleep(1500 * time.Millisecond)
+	// they are not expired yet, extend both
+	assert.NoError(t, tl.RefreshLocks(jobID, twoTargets))
+	time.Sleep(1 * time.Second)
+	// if they were refreshed properly, they are still valid and attempts to get them must fail
+	assert.Error(t, tl.Lock(jobID+1, []*target.Target{&targetOne}))
+	assert.Error(t, tl.Lock(jobID+1, []*target.Target{&targetTwo}))
+}
+
+func TestLockingTransactional(t *testing.T) {
+	tl.ResetAllLocks()
+	// lock the second target
+	assert.NoError(t, tl.Lock(jobID, []*target.Target{&targetTwo}))
+	// try to lock both with another owner (this fails as expected)
+	assert.Error(t, tl.Lock(jobID+1, twoTargets))
+	// API says target one should remain unlocked because Lock() is transactional
+	// this means it can be locked by the first owner
+	assert.NoError(t, tl.Lock(jobID, []*target.Target{&targetOne}))
+}
