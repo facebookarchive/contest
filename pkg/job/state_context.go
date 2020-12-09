50baf0cfc19adafd2a56d0fdd6bba295b0c08957
Ilya Arzamartsev <ilyaarzamartsev@fb.com>
2020-12-09 17:39:28 +0000

    Implementation of StateContext that is intended to remove CancelCh/PauseCh channels.
    Current implementation maintains CancelCh/PauseCh in order to make an step-by-step usage of StateCtx by multiple plugins.

---
+++ pkg/job/state_context.go
@@ -0,0 +1,101 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package job
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"sync"
+	"time"
+)
+
+// ErrCanceled is returned by Context.Err when the context was canceled
+var ErrCanceled = errors.New("job is canceled")
+
+// ErrPaused is returned by Context.Err when the context was paused
+var ErrPaused = errors.New("job is paused")
+
+type State int
+
+var StateActive State = 0
+var StatePaused State = 1
+var StateCanceled State = 2
+
+type StateContext interface {
+	context.Context
+
+	State() State
+
+	cancel()
+	pause()
+}
+
+func newStateContext() StateContext {
+	return &stateCtx{
+		done:  make(chan struct{}),
+		state: StateActive,
+	}
+}
+
+type stateCtx struct {
+	done chan struct{}
+
+	mu    sync.Mutex
+	state State
+}
+
+func (*stateCtx) Deadline() (deadline time.Time, ok bool) {
+	return
+}
+
+func (c *stateCtx) Value( /*key*/ interface{}) interface{} {
+	return nil
+}
+
+func (c *stateCtx) Done() <-chan struct{} {
+	return c.done
+}
+
+func (c *stateCtx) Err() error {
+	state := c.State()
+	switch state {
+	case StateActive:
+		return nil
+	case StatePaused:
+		return ErrPaused
+	case StateCanceled:
+		return ErrCanceled
+	}
+
+	panic(fmt.Sprintf("stateCtx: internal error: unknown state: %d", state))
+}
+
+func (c *stateCtx) State() State {
+	c.mu.Lock()
+	state := c.state
+	c.mu.Unlock()
+	return state
+}
+
+func (c *stateCtx) setFinalState(state State) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.state != StateActive {
+		return // already canceled or paused
+	}
+	c.state = state
+	close(c.done)
+}
+
+// cancel sets c.state to Canceled, closes c.done
+func (c *stateCtx) cancel() {
+	c.setFinalState(StateCanceled)
+}
+
+func (c *stateCtx) pause() {
+	c.setFinalState(StatePaused)
+}
