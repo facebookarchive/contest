83c47d11c28ab739d1d8c713387cbab66275a7a4
Deomid "rojer" Ryabkov <rojer9@fb.com>
2021-01-18 11:22:37 +0000

    Speed up tests by speeding up build of the mysql container
    GVM builds Go from source, this takes a long time.
    Instead, install and use official Linux binaries.
    
    This shaves off 3 minutes from test runs (8 down to 5).

--- docker/mysql/migration.sh
+++ docker/mysql/migration.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -eo pipefail
 echo "Running database migrations..."
-source ${HOME}/.gvm/scripts/gvm 
-gvm use 1.13
-cd ${GOPATH}/src/github.com/facebookincubator/contest
+cd /home/mysql/contest
 # Migrate main db and integration tests db
 go run tools/migration/rdbms/main.go -dbURI "contest:contest@unix(/var/run/mysqld/mysqld.sock)/contest?parseTime=true" -dir db/rdbms/migration up
 go run tools/migration/rdbms/main.go -dbURI "contest:contest@unix(/var/run/mysqld/mysqld.sock)/contest_integ?parseTime=true" -dir db/rdbms/migration up
