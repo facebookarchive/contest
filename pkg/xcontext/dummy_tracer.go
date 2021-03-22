f0f796f60c4aa676665f2ad706fd90ffd5dac383
Dmitrii Okunev <xaionaro@fb.com>
2021-03-22 11:51:24 +0000

    license(xcontext): Add license headers

--- pkg/xcontext/dummy_tracer.go
+++ pkg/xcontext/dummy_tracer.go
@@ -1,3 +1,8 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 // Package xcontext implements a generic context with integrated logger,
 // metrics, tracer and recoverer.
 package xcontext
