3bb3df75282e930397d7db510eef22ecc5ad1f0a
Dmitrii Okunev <xaionaro@fb.com>
2021-03-29 14:51:18 +0100

    polish(xcontext): Simplify logic to update the logger instance

--- pkg/xcontext/context_atomic.go
+++ pkg/xcontext/context_atomic.go
@@ -10,26 +10,10 @@ import (
 	"unsafe"
 )
 
-func (ctx *ctxValue) loadLoggerInstance() *Logger {
-	return (*Logger)(atomic.LoadPointer((*unsafe.Pointer)((unsafe.Pointer)(&ctx.loggerInstance))))
+func (ctx *ctxValue) loadDebugTools() *debugTools {
+	return (*debugTools)(atomic.LoadPointer((*unsafe.Pointer)((unsafe.Pointer)(&ctx.debugTools))))
 }
 
-func (ctx *ctxValue) storeLoggerInstance(newLogger Logger) {
-	atomic.StorePointer((*unsafe.Pointer)((unsafe.Pointer)(&ctx.loggerInstance)), unsafe.Pointer(&newLogger))
-}
-
-func (ctx *ctxValue) loadMetricsInstance() *Metrics {
-	return (*Metrics)(atomic.LoadPointer((*unsafe.Pointer)((unsafe.Pointer)(&ctx.metricsInstance))))
-}
-
-func (ctx *ctxValue) storeMetricsInstance(newMetrics Metrics) {
-	atomic.StorePointer((*unsafe.Pointer)((unsafe.Pointer)(&ctx.metricsInstance)), unsafe.Pointer(&newMetrics))
-}
-
-func (ctx *ctxValue) loadTracerInstance() *Tracer {
-	return (*Tracer)(atomic.LoadPointer((*unsafe.Pointer)((unsafe.Pointer)(&ctx.tracerInstance))))
-}
-
-func (ctx *ctxValue) storeTracerInstance(newTracer Tracer) {
-	atomic.StorePointer((*unsafe.Pointer)((unsafe.Pointer)(&ctx.tracerInstance)), unsafe.Pointer(&newTracer))
+func (ctx *ctxValue) storeDebugTools(newDebugTools *debugTools) {
+	atomic.StorePointer((*unsafe.Pointer)((unsafe.Pointer)(&ctx.debugTools)), unsafe.Pointer(newDebugTools))
 }
