27a8cb8c17b3c2129f031526fed598ac2b538808
mimir-d <mimir-d@users.noreply.github.com>
2021-09-30 01:57:27 +0100

    Add option SendBinary
    - use SendBinary to transfer a local binary to the remote target
      prior to execution
    - if present, treat the Bin.Path as the local source of the file
    - add cleanup mechanism to delete the remote file after usage

--- plugins/teststeps/exec/transport/ssh_transport.go
+++ plugins/teststeps/exec/transport/ssh_transport.go
@@ -6,14 +6,18 @@
 package transport
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net"
+	"os"
 	"strconv"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/kballard/go-shellquote"
+	"github.com/pkg/sftp"
 	"golang.org/x/crypto/ssh"
 
 	"github.com/facebookincubator/contest/pkg/types"
@@ -28,8 +32,8 @@ type SSHTransportConfig struct {
 	Password     string `json:"password,omitempty"`
 	IdentityFile string `json:"identity_file,omitempty"`
 
-	Timeout types.Duration `json:"timeout,omitempty"`
-	// TODO: add transfer binary to remote option
+	Timeout    types.Duration `json:"timeout,omitempty"`
+	SendBinary bool           `json:"send_binary,omitempty"`
 }
 
 func DefaultSSHTransportConfig() SSHTransportConfig {
@@ -78,30 +82,118 @@ func (st *SSHTransport) Start(ctx xcontext.Context, bin string, args []string) (
 		Timeout:         time.Duration(st.Timeout),
 	}
 
-	// TODO: copy bin if needed
-	// TODO: use agent if needed
-	return startSSHExecProcess(ctx, bin, args, addr, clientConfig)
-}
+	// cleanup mechanism similar to defer, but run after the exec process ends
+	done := make(chan struct{})
+	var defered []func()
 
-type sshExecProcess struct {
-	client        *ssh.Client
-	session       *ssh.Session
-	keepAliveDone chan struct{}
+	go func() {
+		<-done
+		for i := len(defered) - 1; i >= 0; i-- {
+			defered[i]()
+		}
+	}()
 
-	stdout io.Reader
-	stderr io.Reader
-}
-
-func startSSHExecProcess(ctx xcontext.Context, bin string, args []string, addr string, clientConfig ssh.ClientConfig) (ExecProcess, error) {
 	client, err := ssh.Dial("tcp", addr, &clientConfig)
 	if err != nil {
 		return nil, fmt.Errorf("cannot connect to SSH server %s: %v", addr, err)
 	}
 
+	// cleanup the ssh client after the operations have ended
+	defered = append(defered, func() {
+		if err := client.Close(); err != nil {
+			ctx.Warnf("failed to close SSH client: %w", err)
+		}
+	})
+
+	// simple case, binary aready exists on the target
+	if !st.SendBinary {
+		return startSSHExecProcess(ctx, client, bin, args, done)
+	}
+
+	// TODO: use agent if needed
+	remoteBin, err := st.sendFile(ctx, client, bin, 0500)
+	if err != nil {
+		return nil, fmt.Errorf("cannot send binary to remote ssh: %w", err)
+	}
+
+	// cleanup the sent file so we don't leave hanging files around
+	defered = append(defered, func() {
+		ctx.Debugf("cleaning remote file: %s", remoteBin)
+		if err := st.unlinkFile(ctx, client, remoteBin); err != nil {
+			ctx.Warnf("failed to cleanup remote file: %w", err)
+		}
+	})
+
+	return startSSHExecProcess(ctx, client, remoteBin, args, done)
+}
+
+func (st *SSHTransport) sendFile(ctx xcontext.Context, client *ssh.Client, bin string, mode os.FileMode) (string, error) {
+	sftp, err := sftp.NewClient(client)
+	if err != nil {
+		return "", fmt.Errorf("failed to create sftp client: %w", err)
+	}
+	defer sftp.Close()
+
+	remoteBin := fmt.Sprintf("/tmp/exec_bin_%s", uuid.New().String())
+	fout, err := sftp.Create(remoteBin)
+	if err != nil {
+		return "", fmt.Errorf("failed to create sftp file: %w", err)
+	}
+	defer fout.Close()
+
+	fin, err := os.Open(bin)
+	if err != nil {
+		return "", fmt.Errorf("cannot open source bin file: %w", err)
+	}
+	defer fin.Close()
+
+	ctx.Debugf("sending file to remote: %s", remoteBin)
+	_, err = fout.ReadFrom(fin)
+	if err != nil {
+		return "", fmt.Errorf("failed to send file: %w", err)
+	}
+
+	return remoteBin, fout.Chmod(mode)
+}
+
+func (st *SSHTransport) unlinkFile(ctx xcontext.Context, client *ssh.Client, bin string) error {
+	sftp, err := sftp.NewClient(client)
+	if err != nil {
+		return fmt.Errorf("failed to create sftp client: %w", err)
+	}
+	defer sftp.Close()
+
+	return sftp.Remove(bin)
+}
+
+type sshExecProcess struct {
+	session       *ssh.Session
+	keepAliveDone chan struct{}
+
+	stdout io.Reader
+	stderr io.Reader
+
+	done chan<- struct{}
+}
+
+func startSSHExecProcess(ctx xcontext.Context, client *ssh.Client, bin string, args []string, done chan<- struct{}) (ExecProcess, error) {
+	var stdin bytes.Buffer
+	return startSSHExecProcessWithStdin(ctx, client, bin, args, &stdin, done)
+}
+
+func startSSHExecProcessWithStdin(
+	ctx xcontext.Context, client *ssh.Client,
+	bin string, args []string,
+	stdin io.Reader,
+	done chan<- struct{},
+) (ExecProcess, error) {
 	session, err := client.NewSession()
 	if err != nil {
-		return nil, fmt.Errorf("cannot create SSH session to server %s: %v", addr, err)
+		return nil, fmt.Errorf("cannot create SSH session to server: %v", err)
 	}
+
+	// set fds for the remote process
+	session.Stdin = stdin
 
 	stdout, err := session.StdoutPipe()
 	if err != nil {
@@ -114,6 +206,8 @@ func startSSHExecProcess(ctx xcontext.Context, bin string, args []string, addr s
 	}
 
 	cmd := shellquote.Join(append([]string{bin}, args...)...)
+	ctx.Debugf("starting remote binary: %s", cmd)
+
 	if err := session.Start(cmd); err != nil {
 		return nil, fmt.Errorf("failed to start process: %w", err)
 	}
@@ -136,15 +230,15 @@ func startSSHExecProcess(ctx xcontext.Context, bin string, args []string, addr s
 		}
 	}()
 
-	return &sshExecProcess{client, session, keepAliveDone, stdout, stderr}, nil
+	return &sshExecProcess{session, keepAliveDone, stdout, stderr, done}, nil
 }
 
 func (sp *sshExecProcess) Wait(_ xcontext.Context) error {
 	// close these no matter what error we get from the wait
 	defer func() {
+		close(sp.done)
 		close(sp.keepAliveDone)
 	}()
-	defer sp.client.Close()
 	defer sp.session.Close()
 
 	if err := sp.session.Wait(); err != nil {
