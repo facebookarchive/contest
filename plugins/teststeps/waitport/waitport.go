7eeb48189c458257ea0023918853513624e80d13
Andrea Barberio <insomniac@slackware.it>
2021-11-26 14:05:36 +0000

    Added missing copyright headers
    Signed-off-by: Andrea Barberio <insomniac@slackware.it>

--- plugins/teststeps/waitport/waitport.go
+++ plugins/teststeps/waitport/waitport.go
@@ -1,3 +1,8 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 // Package waitport provides an ability of waiting for a port on remote host to be opened for listening
 package waitport
 
